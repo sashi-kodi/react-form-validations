c32806fd28f25450258c20f22b1ceb71e26c2fc5
sashi-kodi <sashi.kodi@gmail.com>
2019-07-14 00:16:38 -0700

    updated the validations

--- src/Form.js
+++ src/Form.js
@@ -13,70 +13,85 @@ class Form extends Component{
             submitBtnDisabled:true
         };
     }
-
-
     handleChange=(e)=>{
+        const field = e.target.name;
         this.setState({
             [e.target.name]:e.target.value
         }, ()=>{
-            this.checkValidity();
-        });
-        
+            if(field=== 'name'){
+                this.validateName();
+            }
+            if(field === 'email'){
+                 this.validateEmail();
+            }
+        });   
     }
+    
     handleSubmit=(e)=>{
         e.preventDefault();
         console.log(this.state);
        
     }
-    checkValidity=()=>{
-      this.validateName();
-      this.validateEmail();
-       if (this.state.errorEmailText || this.state.errorNameText){
-           this.setState({submitBtnDisabled:true});
-       }
-        else{
-              this.setState({submitBtnDisabled:false});
-        }
-    }
-    validateName=()=>{
-         const namePattern =/^[a-zA-Z0-9]{6,15}$/;
-         if(!this.state.name.length){
-             this.setState({errorNameText:'Name is required field'});
-             
-        }
-        else{
-            if(!this.state.name.match(namePattern)){
-                 this.setState({errorNameText:'Invalid user name'}); 
+    
+     validateName=()=>{
+        const pattern=/^([a-zA-Z@]+[0-9]*)$/;
+        let isValid =true;
+         let errormessage="";
+         
+         if(!this.state.name.length){ 
+            errormessage='Name is required field';  
+             isValid=false;
+         }
+        //checking if the username is 8 characters long
+         if(isValid && this.state.name.length<8){
+              errormessage='Name must be atleast 8 characters long'; 
+             isValid=false;
+          }
+            
+         if(isValid && !this.state.name.match(pattern)){
+            errormessage='Name must contain only alpha numeric characters and atleast one letter'; 
+             isValid=false;
             }
-            else{
-                this.setState({errorNameText:''});
-               
-            }
+        //checking if the name includes @
+          if (isValid && !this.state.name.includes('@')){
+             errormessage='Name must contain @'; 
+             isValid=false;
+          }
+          this.setState({errorNameText:errormessage});
+         if (isValid && !this.state.errorEmailText && this.state.email){
+             this.setState({submitBtnDisabled:false})
+         }
+         else{
+             this.setState({submitBtnDisabled:true})
+         }
         }
-        
-    }
+         
     validateEmail=()=>{
         const emailPattern =/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+        let errormessage="";
+        let isValid= true;
         if(!this.state.email.length){
-             this.setState({errorEmailText:'Email address is required field'}); 
+             errormessage='Email address is required field'; 
+            isValid=false;
         }
         else{
-            if(!this.state.email.match(emailPattern)){
-                 this.setState({errorEmailText:'Invalid email address'}); 
-            }
-            else{
-                this.setState({errorEmailText:''});
-                
+            if(isValid && !this.state.email.match(emailPattern)){
+                 errormessage='Invalid email address';
+                isValid= false;
             }
         }
-        
-       
+         this.setState({errorEmailText:errormessage});
+        if (isValid && !this.state.errorNameText && this.state.name){
+             this.setState({submitBtnDisabled:false})
+         }
+         else{
+             this.setState({submitBtnDisabled:true})
+         }
     }
     
     render(){
         return(
-           
-        <form onSubmit={this.handleSubmit}   noValidate style={{marginLeft:'30px'}}>
+        <form onSubmit={this.handleSubmit}   noValidate style={{marginLeft:'50px'}}>
             <TextField
             required
             inputProps={{minLength:6, maxLength:15}}
@@ -88,7 +103,7 @@ class Form extends Component{
             helperText={this.state.errorNameText}
             placeholder="Enter your name"
             margin="normal"
-            onBlur={this.checkValidity}
+            onBlur={this.validateName}
             onChange={this.handleChange}/>
             
             <TextField
@@ -102,10 +117,10 @@ class Form extends Component{
             helperText={this.state.errorEmailText}
             placeholder="Enter email"
             margin="normal"
-            onBlur={this.checkValidity}
+            onBlur={this.validateEmail}
             onChange={this.handleChange}/>
             <br/>
-            <Button  type="submit" size="small" color="primary" disabled={this.state.submitBtnDisabled}>
+            <Button  type="submit" size="small" color="primary" disabled={this.state.submitBtnDisabled }>
               submit
              </Button>
         </form>
