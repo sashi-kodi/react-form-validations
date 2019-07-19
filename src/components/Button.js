90cf816b47c0934cc5e04f802d13c5d5f1962f1c
Sashi Kodi <skodi@svb.com>
2019-07-19 14:13:27 -0700

    first commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import './Button.scss';
-
+import { makeStyles } from '@material-ui/core/styles';
+import * as styles from '../my-components.module.scss';
+console.log(styles);
+console.log(styles['mdc-button']);
+// const useStyles = makeStyles({
+//     root: styles['mdc-button']
+// }
+//   );
+  
 const MyButton = (props)=>{
+    //const classes = useStyles();
+  
     return(
-    <Button classes={{root:'btn'}}>Hello World</Button>
+    <Button classes={{root:'my-components_mdc-button__36ZzM'}}>Hello World</Button>
     )
 }
 export default MyButton;
