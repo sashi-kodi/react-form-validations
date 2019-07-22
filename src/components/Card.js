1c8f87d39d42c4e9af829552397e3520ee83b02e
sashi-kodi <sashi.kodi@gmail.com>
2019-07-21 23:10:56 -0700

    updated the imports

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-import '../my-components.scss';
+
 
 
 const MyCard = (props)=>{
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -15,7 +15,7 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import '../my-components.scss';
+
 import {Link}   from 'react-router-dom';
 
 
